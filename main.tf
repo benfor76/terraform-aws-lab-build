dbd4149c59fd153b233ad5e42c517f5327c0e970
Ben Forrester <ben_forrester@hotmail.com>
2025-08-06 18:02:25 -0400

    changer8 8-6-25

--- main.tf
+++ main.tf
@@ -140,49 +140,6 @@ resource "aws_security_group" "ben_lab_sg" {
   }
 }
 
-# 1. Generate a new RSA private key
-resource "tls_private_key" "key" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-# 2. Create the AWS key pair
-resource "aws_key_pair" "generated_key" {
-  key_name   = "ben1-lab-key-pair"  # Unique key name
-  public_key = tls_private_key.key.public_key_openssh
-}
-
-# 3. Store private key in Secrets Manager
-resource "aws_secretsmanager_secret" "lab_private_key" {
-  name        = "ben1-lab-key-pair/private"  # Secret name
-  description = "Private key for key pair ${aws_key_pair.generated_key.key_name}"
-}
-
-resource "aws_secretsmanager_secret_version" "lab_private_key" {
-  secret_id     = aws_secretsmanager_secret.lab_private_key.id
-  secret_string = tls_private_key.key.private_key_pem  # PEM-encoded private key
-}
-
-# 4. Retrieve the private key from Secrets Manager
-data "aws_secretsmanager_secret_version" "retrieved" {
-  secret_id = aws_secretsmanager_secret.lab_private_key.id
-  depends_on = [aws_secretsmanager_secret_version.lab_private_key]
-}
-
-## Output results
-output "key_pair_name" {
-  value = aws_key_pair.generated_key.key_name
-}
-
-output "secret_arn" {
-  value = aws_secretsmanager_secret.lab_private_key.arn
-}
-
-output "retrieved_private_key" {
-  value     = data.aws_secretsmanager_secret_version.retrieved.secret_string
-  sensitive = true  # Marks output as sensitive in console
-}
-
 data "aws_ami" "rhelami" {
   most_recent = true
   owners      = ["309956199498"]  # Official Red Hat account
